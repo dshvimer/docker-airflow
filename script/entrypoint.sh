e4d1a55614fa59b096b73cbbc1a934f0c3e15aa1
David Shvimer <david@Davids-MacBook-Pro.local>
2019-09-16 19:23:41 -0400

    Make updates for running k8s

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -44,19 +44,19 @@ else
     REDIS_PREFIX=
 fi
 
-wait_for_port() {
-  local name="$1" host="$2" port="$3"
-  local j=0
-  while ! nc -z "$host" "$port" >/dev/null 2>&1 < /dev/null; do
-    j=$((j+1))
-    if [ $j -ge $TRY_LOOP ]; then
-      echo >&2 "$(date) - $host:$port still not reachable, giving up"
-      exit 1
-    fi
-    echo "$(date) - waiting for $name... $j/$TRY_LOOP"
-    sleep 5
-  done
-}
+# wait_for_port() {
+#   local name="$1" host="$2" port="$3"
+#   local j=0
+#   while ! nc -z "$host" "$port" >/dev/null 2>&1 < /dev/null; do
+#     j=$((j+1))
+#     if [ $j -ge $TRY_LOOP ]; then
+#       echo >&2 "$(date) - $host:$port still not reachable, giving up"
+#       exit 1
+#     fi
+#     echo "$(date) - waiting for $name... $j/$TRY_LOOP"
+#     sleep 5
+#   done
+# }
 
 if [ "$AIRFLOW__CORE__EXECUTOR" != "SequentialExecutor" ]; then
   AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
